754bef8614b5d3e7aa35e7f57735a35ac9e2c2bb
wissem90 <35082331+wissem90@users.noreply.github.com>
2018-01-03 23:37:08 +0100

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,9 @@
+CASC_PATH = './haarcascade_files/haarcascade_frontalface_default.xml'
+SIZE_FACE = 48
+EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
+SAVE_DIRECTORY = './data/'
+SAVE_MODEL_FILENAME = 'model_1_nimish.tflearn'
+SAVE_DATASET_IMAGES_FILENAME = 'data_set_fer2013.npy'
+SAVE_DATASET_LABELS_FILENAME = 'data_labels_fer2013.npy'
+SAVE_DATASET_IMAGES_TEST_FILENAME = 'test_set_fer2013.npy'
+SAVE_DATASET_LABELS_TEST_FILENAME = 'test_labels_fer2013.npy'
